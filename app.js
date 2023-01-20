5b79c7c50e88b60c94979d7c607382991c33d3da
rtong2 <52456933+rtong2@users.noreply.github.com>
2023-01-20 17:14:58 -0500

    Update app.js
    Add another step

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CI/CD demo - after the pipeline is built'));
+app.get('/', (req, res) => res.send('CI/CD demo - after the pipeline is built - Now add the manual review stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
